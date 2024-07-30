45f4bc1e90ba97704ef8b2bec1680e732a2be50f
Caio Martins <icaiomarxm@gmail.com>
2024-07-30 12:20:15 -0300

    update: removing unused imports

--- singularity/server/lifespan.py
+++ singularity/server/lifespan.py
@@ -1,7 +1,6 @@
 from contextlib import asynccontextmanager
 
-from sqlmodel import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 
 from singularity.database.engine import get_session
 from singularity.database.init_db import init_db
