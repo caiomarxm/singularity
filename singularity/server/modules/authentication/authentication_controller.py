424ec07e88a2e7e088e73380ada033225712b420
Caio Martins <icaiomarxm@gmail.com>
2024-07-30 13:37:32 -0300

    improving readability

--- singularity/server/modules/authentication/authentication_controller.py
+++ singularity/server/modules/authentication/authentication_controller.py
@@ -7,7 +7,7 @@ from singularity.authentication.oauth2.token_manager import TokenManager
 from singularity.authentication.security.password_manager import PasswordManager
 
 from singularity.database.engine import get_session
-from singularity.database.repositories.rbac.user_repository import read_user
+from singularity.database.repositories.rbac.user_repository import read_user_from_email
 
 from singularity.server.modules.authentication.authentication_schemas import (
     LoginResponseData,
@@ -22,15 +22,13 @@ def login_with_username_and_password(
     user_login: Annotated[OAuth2PasswordRequestForm, Depends()],
     session: Session = Depends(get_session),
 ):
-    user = read_user(session=session, user_id=user_login.username)
+    user = read_user_from_email(session=session, user_email=user_login.username)
 
     if not user or not PasswordManager.verify_password(
         user.hashed_password, user_login.password
     ):
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid credentials",
-            headers={"WWW-Authenticate": "Bearer"},
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
         )
 
     access_token_encoded = TokenManager.create_access_token(
