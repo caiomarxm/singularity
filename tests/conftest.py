7e205ba5e0ae6c49070f14d350ccea1539c06066
Caio Martins <icaiomarxm@gmail.com>
2024-07-30 12:53:40 -0300

    test: adding fixture to user_with_hashed_password

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from singularity.database.models.rbac import *
 
 from sqlmodel import SQLModel, Session, create_engine
 
+from singularity.authentication.security.password_manager import PasswordManager
 
 from singularity.database.models.rbac import User, UserCreate
 from singularity.database.repositories.rbac.user_repository import create_user
@@ -25,5 +26,16 @@ def session():
 def user(session: Session) -> User:
     user_create = UserCreate(
         email="testuser@example.com", hashed_password="testhashedpassword"
+    )
+    return create_user(session=session, user_in=user_create)
+
+
+@pytest.fixture
+def user_with_hashed_password(session: Session) -> User:
+    user_create = UserCreate(
+        email="testuser@example.com",
+        hashed_password=PasswordManager.hash_password(
+            "testpassword"
+        ),  # Make sure to use the same hashing logic as in your application
     )
     return create_user(session=session, user_in=user_create)
