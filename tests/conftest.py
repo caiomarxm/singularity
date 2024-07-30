50c1a49021f885e4d94b3c0eae7ac1172364a539
Caio Martins <icaiomarxm@gmail.com>
2024-07-30 11:55:36 -0300

    test: refactoring user fixture to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,9 @@ from singularity.database.models.rbac import *
 from sqlmodel import SQLModel, Session, create_engine
 
 
+from singularity.database.models.rbac import User, UserCreate
+from singularity.database.repositories.rbac.user_repository import create_user
+
 FIXTURE_DATABASE_URL = "sqlite:///:memory:"
 
 
@@ -16,3 +19,11 @@ def session():
     SQLModel.metadata.create_all(bind=engine)
     with Session(engine) as session:
         yield session
+
+
+@pytest.fixture
+def user(session: Session) -> User:
+    user_create = UserCreate(
+        email="testuser@example.com", hashed_password="testhashedpassword"
+    )
+    return create_user(session=session, user_in=user_create)
