3e2c48c164c86f92b264a1e87b94a8543e03e5d9
Caio Martins <icaiomarxm@gmail.com>
2024-07-30 13:10:47 -0300

    feat: adding authentication route

--- singularity/server/routes/api_router.py
+++ singularity/server/routes/api_router.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
 
+from singularity.server.modules.authentication import authentication_controller
+
 api_router = APIRouter(prefix="")
 
-# api_router.include_router()
+api_router.include_router(
+    authentication_controller.router, prefix="/login", tags="login"
+)
