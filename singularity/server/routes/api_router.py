3bbb49f8e2cdfa2d4d50dd288d29f17cfabc0207
Caio Martins <icaiomarxm@gmail.com>
2024-07-31 15:52:47 -0300

    feat: adding rbac router to API router

--- singularity/server/routes/api_router.py
+++ singularity/server/routes/api_router.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 
 from singularity.server.modules.authentication import authentication_controller
+from singularity.server.modules.rbac import rbac_controller
 
 api_router = APIRouter(prefix="")
 
 api_router.include_router(
-    authentication_controller.router, prefix="/login", tags="login"
+    authentication_controller.router, prefix="/login", tags=["login"]
 )
+
+api_router.include_router(rbac_controller.router, prefix="/rbac", tags=["rbac"])
