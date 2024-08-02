da691f5f49fbf6376252f21c0d1d26ab7e2b0559
Caio Martins <icaiomarxm@gmail.com>
2024-08-02 05:08:58 -0300

    feat: adding rbac middleware and route_permission mapping

--- singularity/server/server.py
+++ singularity/server/server.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 
 from singularity.server.lifespan import lifespan
 from singularity.server.routes.api_router import api_router
+from singularity.server.middleware.rbac_middleware import RBACMiddleware
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(RBACMiddleware)
 
 app.include_router(api_router)
