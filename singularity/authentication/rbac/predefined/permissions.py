8e0bb5b5ed5d2072eaf31982559bed72eecece38
Caio Martins <icaiomarxm@gmail.com>
2024-08-05 05:34:28 -0300

    feat: predefined permissions

--- singularity/authentication/rbac/predefined/permissions.py
+++ singularity/authentication/rbac/predefined/permissions.py
@@ -10,21 +10,77 @@ class UserPermissions(BaseModel):
     update_self: Permission = Permission(
         name="user.self.update", description="Can update details of self user"
     )
+    create_user: Permission = Permission(
+        name="user.create", description="Can create or invite a new user"
+    )
+    update_user: Permission = Permission(
+        name="user.create", description="Can update an existing user"
+    )
+    list_users: Permission = Permission(
+        name="user.list",
+        description="Can view all existing user within a level of permission",
+    )
+    view_user: Permission = Permission(
+        name="user.detail",
+        description="Can view details of an existing user within a level of permission",
+    )
+    delete_user: Permission = Permission(
+        name="user.delete", description="Can delete an existing user"
+    )
 
 
-class SquadMembershipsLevelPermissions(BaseModel):
-    pass
+class SquadMembershipsPermissions(BaseModel):
+    add_user_to_squad: Permission = Permission(
+        name="squad.membership.create",
+        description="Can add users to an organization",
+    )
+    remove_user_from_squad: Permission = Permission(
+        name="squad.membership.delete",
+        description="Can add users to an organization",
+    )
 
 
-class SquadLevelPermissions(BaseModel):
-    pass
+class SquadPermissions(BaseModel):
+    create_squad_in_organization: Permission = Permission(
+        name="squad.create",
+        description="Can create squads within the Organization",
+    )
+    update_squad_in_organization: Permission = Permission(
+        name="squad.update",
+        description="Can update squads within the Organization",
+    )
+    list_squads_in_organization: Permission = Permission(
+        name="squad.list",
+        description="Can update squads within the Organization",
+    )
+    detail_squad_in_organization: Permission = Permission(
+        name="squad.detail",
+        description="Can update squads within the Organization",
+    )
+    delete_squad_in_organization: Permission = Permission(
+        name="squad.delete",
+        description="Can delete squads within the Organization",
+    )
 
 
-class OrganizationMembershipLevelPermissions(BaseModel):
-    pass
+class OrganizationMembershipPermissions(BaseModel):
+    add_user_to_organization: Permission = Permission(
+        name="organization.membership.create",
+        description="Can add users to an organization",
+    )
+    remove_user_from_organization: Permission = Permission(
+        name="organization.membership.delete",
+        description="Can add users to an organization",
+    )
 
 
-class OrganizationLevelPermissions(BaseModel):
+class OrganizationPermissions(BaseModel):
+    create_organization: Permission = Permission(
+        name="organization.create", description="Can create new organizations"
+    )
+    delete_organization: Permission = Permission(
+        name="organization.delete", description="Can delete organizations"
+    )
     view_organization: Permission = Permission(
         name="organization.view",
         description="Can view details of it's own organization",
@@ -33,28 +89,51 @@ class OrganizationLevelPermissions(BaseModel):
         name="organization.update",
         description="Can update details of it's own organization",
     )
-    create_squad_in_organization: Permission = Permission(
-        name="organization.squads.create",
-        description="Can create squads within the Organization",
+
+
+class RoleEntityPermissions(BaseModel):
+    create_custom_role: Permission = Permission(
+        name="role.create", description="Can create new custom roles"
     )
-    update_squad_in_organization: Permission = Permission(
-        name="organization.squads.update",
-        description="Can update squads within the Organization",
+    delete_custom_role: Permission = Permission(
+        name="role.delete", description="Can delete custom roles"
     )
-    list_squads_in_organization: Permission = Permission(
-        name="organization.squads.list",
-        description="Can update squads within the Organization",
+    list_roles: Permission = Permission(
+        name="role.list",
+        description="Can list roles",
     )
-    detail_squad_in_organization: Permission = Permission(
-        name="organization.squads.detail",
-        description="Can update squads within the Organization",
+    view_role: Permission = Permission(
+        name="role.view",
+        description="Can view details of a role",
+    )
+    update_custom_role: Permission = Permission(
+        name="role.update",
+        description="Can update details of a custom role",
     )
 
 
-class SuperadminLevelPermissions(BaseModel):
-    create_organization: Permission = Permission(
-        name="organization.create", description="Can create new organizations"
+class PermissionEntityPermissions(BaseModel):
+    list_permissions: Permission = Permission(
+        name="permission.list",
+        description="Can list permissions",
     )
-    delete_organization: Permission = Permission(
-        name="organization.delete", description="Can delete organizations"
+    view_role: Permission = Permission(
+        name="permission.view",
+        description="Can view details of a permission",
     )
+
+
+class PredefinedPermissions:
+    roles = RoleEntityPermissions()
+    permissions = PermissionEntityPermissions()
+
+    organization = OrganizationPermissions()
+    organization_membership = OrganizationMembershipPermissions()
+
+    squad = SquadPermissions()
+    squad_membership = SquadMembershipsPermissions()
+
+    user = UserPermissions()
+
+
+PREDEFINED_PERMISSIONS = PredefinedPermissions()
