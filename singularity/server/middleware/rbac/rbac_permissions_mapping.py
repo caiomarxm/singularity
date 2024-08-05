c29d664e75448fceb8076aedb525e72e46b1613b
Caio Martins <icaiomarxm@gmail.com>
2024-08-05 05:35:11 -0300

    feat: using PREDEFINED_PERMISSIONS to fetch permissions for each route

--- singularity/server/middleware/rbac/rbac_permissions_mapping.py
+++ singularity/server/middleware/rbac/rbac_permissions_mapping.py
@@ -1,19 +1,23 @@
+from singularity.authentication.rbac.predefined.permissions import (
+    PREDEFINED_PERMISSIONS,
+)
+
 RBAC_ROUTE_PERMISSION_MAPPING = {
     "rbac": {
         "roles": {
             "": {
                 "level": "admin",
                 "permissions": {
-                    "GET": "rbac.roles.list",
-                    "POST": "rbac.roles.create",
+                    "GET": PREDEFINED_PERMISSIONS.roles.list_roles.name,
+                    "POST": PREDEFINED_PERMISSIONS.roles.create_custom_role.name,
                 },
             },
             "{role_id}": {
                 "level": "admin",
                 "permissions": {
-                    "GET": "rbac.roles.detail",
-                    "PUT": "rbac.roles.update",
-                    "DELETE": "rbac.roles.delete",
+                    "GET": PREDEFINED_PERMISSIONS.roles.view_role.name,
+                    "PUT": PREDEFINED_PERMISSIONS.roles.update_custom_role.name,
+                    "DELETE": PREDEFINED_PERMISSIONS.roles.delete_custom_role.name,
                 },
             },
         },
