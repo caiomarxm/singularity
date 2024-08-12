599f3a9890c101cd9ee21e3a52742d0b05b66d25
Caio Martins <icaiomarxm@gmail.com>
2024-08-12 03:58:31 -0300

    update: removing bootstrap db from lifespan

--- singularity/server/lifespan.py
+++ singularity/server/lifespan.py
@@ -3,14 +3,12 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from singularity.database.engine import get_session
-from singularity.database.init_db import init_db
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    session = next(get_session())
-    init_db(session=session)
+    # session = next(get_session())
 
-    # Check services sucha as Redis, RabbitMQ ...
+    # Check services such as Redis, RabbitMQ ...
 
     yield
