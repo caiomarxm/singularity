6945afd173ab11edf3f2e329918a98c4220c8422
Caio Martins <icaiomarxm@gmail.com>
2024-08-12 03:47:58 -0300

    update: changing from Permission to PermissionCreate to bootstrap

--- singularity/authentication/rbac/predefined/permissions.py
+++ singularity/authentication/rbac/predefined/permissions.py
@@ -1,123 +1,123 @@
 from pydantic import BaseModel
 
-from singularity.database.models.rbac import Permission
+from singularity.database.models.rbac import PermissionCreate
 
 
 class UserPermissions(BaseModel):
-    view_self: Permission = Permission(
+    view_self: PermissionCreate = PermissionCreate(
         name="user.self.view", description="Can view details of self user"
     )
-    update_self: Permission = Permission(
+    update_self: PermissionCreate = PermissionCreate(
         name="user.self.update", description="Can update details of self user"
     )
-    create_user: Permission = Permission(
+    create_user: PermissionCreate = PermissionCreate(
         name="user.create", description="Can create or invite a new user"
     )
-    update_user: Permission = Permission(
+    update_user: PermissionCreate = PermissionCreate(
         name="user.create", description="Can update an existing user"
     )
-    list_users: Permission = Permission(
+    list_users: PermissionCreate = PermissionCreate(
         name="user.list",
         description="Can view all existing user within a level of permission",
     )
-    view_user: Permission = Permission(
+    view_user: PermissionCreate = PermissionCreate(
         name="user.detail",
         description="Can view details of an existing user within a level of permission",
     )
-    delete_user: Permission = Permission(
+    delete_user: PermissionCreate = PermissionCreate(
         name="user.delete", description="Can delete an existing user"
     )
 
 
 class SquadMembershipsPermissions(BaseModel):
-    add_user_to_squad: Permission = Permission(
+    add_user_to_squad: PermissionCreate = PermissionCreate(
         name="squad.membership.create",
         description="Can add users to an organization",
     )
-    remove_user_from_squad: Permission = Permission(
+    remove_user_from_squad: PermissionCreate = PermissionCreate(
         name="squad.membership.delete",
         description="Can add users to an organization",
     )
 
 
 class SquadPermissions(BaseModel):
-    create_squad_in_organization: Permission = Permission(
+    create_squad_in_organization: PermissionCreate = PermissionCreate(
         name="squad.create",
         description="Can create squads within the Organization",
     )
-    update_squad_in_organization: Permission = Permission(
+    update_squad_in_organization: PermissionCreate = PermissionCreate(
         name="squad.update",
         description="Can update squads within the Organization",
     )
-    list_squads_in_organization: Permission = Permission(
+    list_squads_in_organization: PermissionCreate = PermissionCreate(
         name="squad.list",
         description="Can update squads within the Organization",
     )
-    detail_squad_in_organization: Permission = Permission(
+    detail_squad_in_organization: PermissionCreate = PermissionCreate(
         name="squad.detail",
         description="Can update squads within the Organization",
     )
-    delete_squad_in_organization: Permission = Permission(
+    delete_squad_in_organization: PermissionCreate = PermissionCreate(
         name="squad.delete",
         description="Can delete squads within the Organization",
     )
 
 
 class OrganizationMembershipPermissions(BaseModel):
-    add_user_to_organization: Permission = Permission(
+    add_user_to_organization: PermissionCreate = PermissionCreate(
         name="organization.membership.create",
         description="Can add users to an organization",
     )
-    remove_user_from_organization: Permission = Permission(
+    remove_user_from_organization: PermissionCreate = PermissionCreate(
         name="organization.membership.delete",
         description="Can add users to an organization",
     )
 
 
 class OrganizationPermissions(BaseModel):
-    create_organization: Permission = Permission(
+    create_organization: PermissionCreate = PermissionCreate(
         name="organization.create", description="Can create new organizations"
     )
-    delete_organization: Permission = Permission(
+    delete_organization: PermissionCreate = PermissionCreate(
         name="organization.delete", description="Can delete organizations"
     )
-    view_organization: Permission = Permission(
+    view_organization: PermissionCreate = PermissionCreate(
         name="organization.view",
         description="Can view details of it's own organization",
     )
-    update_organization: Permission = Permission(
+    update_organization: PermissionCreate = PermissionCreate(
         name="organization.update",
         description="Can update details of it's own organization",
     )
 
 
 class RoleEntityPermissions(BaseModel):
-    create_custom_role: Permission = Permission(
+    create_custom_role: PermissionCreate = PermissionCreate(
         name="role.create", description="Can create new custom roles"
     )
-    delete_custom_role: Permission = Permission(
+    delete_custom_role: PermissionCreate = PermissionCreate(
         name="role.delete", description="Can delete custom roles"
     )
-    list_roles: Permission = Permission(
+    list_roles: PermissionCreate = PermissionCreate(
         name="role.list",
         description="Can list roles",
     )
-    view_role: Permission = Permission(
+    view_role: PermissionCreate = PermissionCreate(
         name="role.view",
         description="Can view details of a role",
     )
-    update_custom_role: Permission = Permission(
+    update_custom_role: PermissionCreate = PermissionCreate(
         name="role.update",
         description="Can update details of a custom role",
     )
 
 
 class PermissionEntityPermissions(BaseModel):
-    list_permissions: Permission = Permission(
+    list_permissions: PermissionCreate = PermissionCreate(
         name="permission.list",
         description="Can list permissions",
     )
-    view_role: Permission = Permission(
+    view_permission: PermissionCreate = PermissionCreate(
         name="permission.view",
         description="Can view details of a permission",
     )
