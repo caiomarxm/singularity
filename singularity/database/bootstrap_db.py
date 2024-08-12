9766214472a292178dc2b14fba71e1f32f5f15ec
Caio Martins <icaiomarxm@gmail.com>
2024-08-12 03:57:01 -0300

    refactor: using create_permissions_batch

--- singularity/database/bootstrap_db.py
+++ singularity/database/bootstrap_db.py
@@ -1,5 +1,4 @@
-from sqlmodel import Session, select
-from typing import List
+from sqlmodel import Session
 
 from singularity.authentication.security.password_manager import PasswordManager
 from singularity.authentication.rbac.predefined.permissions import (
@@ -11,43 +10,20 @@ from singularity.database.repositories.rbac.user_repository import (
     read_user_from_email,
 )
 from singularity.database.repositories.rbac.permission_repository import (
-    create_permission,
+    create_permissions_batch,
 )
-from singularity.database.models.rbac import UserCreate, Permission
+from singularity.database.models.rbac import UserCreate
 
 from singularity.settings.settings import settings
 
 
 def bootstrap_permissions(session: Session) -> None:
-    # Collect all predefined permissions from the PREDEFINED_PERMISSIONS structure
     predefined_permissions = []
-    permission_names = []
     for category_name, category_permissions in PREDEFINED_PERMISSIONS.__dict__.items():
         for permission_name, permission in category_permissions.__dict__.items():
             predefined_permissions.append(permission)
-            permission_names.append(permission_name)
 
-    # Fetch existing permissions from the database
-    existing_permissions = session.exec(
-        select(Permission).where(Permission.name.in_(permission_names))
-    ).all()
-
-    existing_permission_names = {perm.name for perm in existing_permissions}
-
-    # Filter out permissions that already exist
-    permissions_to_create: List[Permission] = [
-        perm
-        for perm in predefined_permissions
-        if perm.name not in existing_permission_names
-    ]
-
-    # Add new permissions to the session and commit in a batch
-    if permissions_to_create:
-        for permission in permissions_to_create:
-            try:
-                create_permission(session=session, permission_in=permission)
-            except ValueError:
-                pass
+    create_permissions_batch(session=session, permissions=predefined_permissions)
 
 
 def bootstrap_db_defaults(session: Session) -> None:
